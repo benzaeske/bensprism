d41770e613041463c99f7c4173edf553968296c8
benzaeske <32627698+benzaeske@users.noreply.github.com>
2025-03-06 13:46:11 -0700

    working on Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,13 @@ import '../styles/globals.css';
 import '../styles/globalStyles.scss';
 import type { AppProps } from "next/app";
 
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
